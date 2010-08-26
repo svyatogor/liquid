f299f8b7ff6ea102bfb3d1e28fc118c445626a48
dinedine <didier.lafforgue@gmail.com>
2010-08-26 12:40:49 +0200

    context used in the parsing time was not the same instance all the time causing some bugs

--- lib/liquid/template.rb
+++ lib/liquid/template.rb
@@ -55,7 +55,7 @@ module Liquid
     # Parse source code.
     # Returns self for easy chaining
     def parse(source, context = {})
-      @root = Document.new(tokenize(source), context.merge(:template => self))
+      @root = Document.new(tokenize(source), context.merge!(:template => self))
       self
     end
 
