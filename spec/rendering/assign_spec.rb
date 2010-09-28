49e718e36b061b4162591bc7a5f3d0269e2cd17b
Jacques Crocker <railsjedi@gmail.com>
2010-09-28 07:14:42 -0700

    Converting Block, Assign, and Capture to specs

---
+++ spec/rendering/assign_spec.rb
@@ -0,0 +1,31 @@
+require 'spec_helper'
+
+module Liquid
+  describe "Liquid Rendering" do
+    describe "Assignment" do
+      let(:template) do
+        Template.parse(eval(subject))
+      end
+
+      context %|with 'values' => ["foo", "bar", "baz"]| do
+        let(:render_options) do
+          {
+            'values' => ["foo", "bar", "baz"]
+          }
+        end
+
+        describe %|"{% assign foo = values %}.{{ foo[0] }}."| do
+          it{ template.render(render_options).should == ".foo." }
+        end
+
+        describe %|"{% assign foo = values %}.{{ foo[1] }}."| do
+          it{ template.render(render_options).should == ".bar." }
+        end
+
+        describe %|"{% assign foo = values %}.{{ foo[2] }}."| do
+          it{ template.render(render_options).should == ".baz." }
+        end
+      end
+    end
+  end
+end
